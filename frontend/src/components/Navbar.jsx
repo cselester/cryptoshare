f5228b04b182565fc344002bc271fa7b9892b893
lester <tushartripathi2002@gmail.com>
2026-04-06 23:21:14 +0530

    Title bar changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <div className="navbar-brand">Cryptoshare</div>
+      <div className="navbar-brand">
+        <img src={logo} alt="Cryptoshare logo" className="navbar-logo" />
+        <span>Cryptoshare</span>
+      </div>
       <div className="navbar-links">
         <Link to="/">Create Secret</Link>
       </div>
